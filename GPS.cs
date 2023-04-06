8a687fced156a27255457e69dc4012f082a73d10
Melisa Nguyen <72332347+melngu11@users.noreply.github.com>
2023-04-06 01:58:40 -0500

    Add files via upload

---
+++ GPS.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPS : MonoBehaviour
+{
+    // Public variable to reference the Text UI element for displaying coordinates
+    public Text coordinatesText;
+
+    // Base origin coordinates
+    public float baseLatitude;
+    public float baseLongitude;
+    public float baseAltitude;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the XYZ position of the game object
+        Vector3 position = transform.position;
+
+        // Convert the XYZ position to GPS coordinates based on the base origin
+        float latitude = baseLatitude + position.z * 111111f;
+        float longitude = baseLongitude + position.x * 111111f;
+        float altitude = baseAltitude + position.y;
+
+        // Format the Unity coordinates as a string
+        string unityCoordinatesString = string.Format("Unity Coordinates\nX: {0}\nY: {1}\nZ: {2}", position.x, position.y, position.z);
+
+        // Format the GPS coordinates as a string
+        string gpsCoordinatesString = string.Format("GPS Coordinates\nLatitude: {0}\nLongitude: {1}\nAltitude: {2}", latitude, longitude, altitude);
+
+        // Combine the Unity and GPS coordinates strings
+        string coordinatesString = unityCoordinatesString + "\n\n" + gpsCoordinatesString;
+
+        // Update the Text UI element with the combined coordinates string
+        coordinatesText.text = coordinatesString;
+    }
+}
